00919ab496a6ee855cd6f9265ae45cc690d8d186
kuznetsovkeril <kuznetsovkeril@gmail.com>
2023-10-11 23:40:52 +0300

    add register fixture

--- tests/registration/test_register.py
+++ tests/registration/test_register.py
@@ -5,37 +5,31 @@ from utilities.checking import Checking
 from utilities.utilities import Instruments
 
 
+@pytest.fixture
+def register(ref_id, slug):
+    email = Instruments.generate_unique_email()  # генерация уникального email
+    result_reg = Dexart_api.register_with_mail(email, ref_id, partner_slug=slug)
+    # проверка статус кода
+    print(f'Фактический статус код: {result_reg.status_code}')
+    Checking.check_status_code(result_reg, 200)
+    # получение токена авторизации
+    auth_token = Checking.get_json_value(result_reg, "data", "token")
+    print(f'Токен авторизации: {auth_token}')
+    yield auth_token  # возвращает токен авторизации перед каждым тестом
+
+
 class TestRegisterRefDexart:
     """Регистрация пользователя в Dexart в разных реф программах"""
 
     # успешная регистрация в нативной рефке с корректным ref_id, без рефки, под неверной рефкой
-    @pytest.mark.parametrize("ref_id, expected_ref_id, test_name", [(789432943, 789432943, "Correct referral id"),
-                                                                    (1337, 1, "Wrong referral id"),
-                                                                    (None, 1, "No referral id")])
-    def test_referral_dexart(self, ref_id, expected_ref_id, test_name):
-        print("Register post method")
-        email = Instruments.generate_unique_email()  # генерация уникального email
-        print(f'Почта нового юзера: {email}')
-        result_reg = Dexart_api.register_with_mail(email, ref_id, partner_slug=None)
-        # проверка статус кода
-        print(f'Фактический статус код: {result_reg.status_code}')
-        Checking.check_status_code(result_reg, 200)
-
-        """Получение токена авторизации из запроса регистрации"""
-        auth_token = Checking.get_json_value(result_reg, "data", "token")
-        print(f'Токен авторизации: {auth_token}')
-
-        """Проверка успешной регистрации у нужного рефа"""
-        result_reg_check = Dexart_api.user_referral_info(auth_token)
-        expected_sponsor_ref_id = expected_ref_id  # ожидаемое значение после реги в поле реф id в блоке спонсора
+    @pytest.mark.parametrize("ref_id, slug, expected_ref_id, test_name", [(789432943, None, 789432943, "Correct referral id"),
+                                                                    (1337, None, 1, "Wrong referral id"),
+                                                                    (None, None, 1, "No referral id")])
+    def test_referral_dexart(self, register, ref_id, slug, expected_ref_id, test_name):
+        # Проверка успешной регистрации у нужного рефа
+        result_reg_check = Dexart_api.user_referral_info(register)
         Checking.check_json_value_3(result_reg_check, "data", "ref", "id", expected_ref_id)
 
-    def test_partner_referral(self):
-        pass
-        # тут будет кейс регистрации у партнера SpacAd, Oton, у которого нет настроек по регистрации
-
-    def test_ref_client_group(self):
-        pass
 
         # проверка что у дексартовца был создан маркет ид
         # проверка что у пратнера есть поле с партнером
