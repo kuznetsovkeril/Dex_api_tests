a96370f12230d1022f62f53083681d3137098ed5
kuznetsovkeril <kuznetsovkeril@gmail.com>
2023-11-01 22:10:38 +0300

    fix signature and schedule tests

--- tests/spacAd/test_watch_signature.py
+++ tests/spacAd/test_watch_signature.py
@@ -3,7 +3,6 @@ import random
 import string
 
 import time
-from datetime import datetime, timedelta
 
 import pytest
 
@@ -25,6 +24,13 @@ class TestWatchSignature:
         signature = hashlib.md5(data.encode()).hexdigest()
         return signature + random_string_md5
 
+    @staticmethod
+    def check_watch_result(result, status_code, expected):
+        Checking.check_status_code(result, status_code)
+        actual = Getters.get_json_field_value_0(result, "message")
+        expected_result = expected
+        Checking.assert_values(expected_result, actual)
+
     """Проверка подписи по позитивным сценариям"""
 
     @pytest.mark.parametrize("email, salt, test_name", [
@@ -32,28 +38,19 @@ class TestWatchSignature:
         (EMAIL_SPACAD_WHITELISTED, ANDROID_SOLT, "Test signature for ANDROID"),
         (EMAIL_SPACAD_WHITELISTED, IOS_SOLT, "Test signature for IOS")])
     def test_signature_watch_ad_positive(self, set_spacad_ad, email, salt, test_name):
-        time.sleep(60)  # чтобы можно было собрать следующую монету
         result = Spacad_api.watch(email=email, signature=self.generate_signature(email=email, salt=salt))
         Checking.check_status_code(result, 200)
-        actual = Getters.get_json_field_value_2(result, field_name_1="data", field_name_2="max_limit")
-        expected = "5"
-        Checking.assert_values(expected, actual)
+        expected_fields = ['id', 'coins', 'max_limit', 'min_limit', 'coins_left', 'progress', 'expire_at', 'created_at']
+        Checking.check_json_fields_in_v2(result, "data", expected_fields)
+        time.sleep(60)  # чтобы можно было собрать следующую монету
 
     """Проверка подписи по негативным сценариям"""
 
-    @pytest.mark.parametrize("email, salt", [(EMAIL_SPACAD_WHITELISTED, "Wa$ZFkWFGywHx3LsFRPq")])
-    def test_wrong_signature_watch_ad(self, set_spacad_ad, email, salt):
+    @pytest.mark.parametrize("email, salt, expected, test_name", [
+        (EMAIL_SPACAD_WHITELISTED, "Wa$ZFkWFGywHx3LsFRPq", "An application update is required", "Test wrong signature"),
+        (EMAIL_SPACAD_WHITELISTED, SPACAD_KYC_SIGNATURE, "An application update is required", "Test another user signature"),
+        (EMAIL_SPACAD_WHITELISTED, None, "Update application or refresh the page.", "Test no signature")])
+    def test_signature_watch_ad_negative(self, set_spacad_ad, email, salt, expected, test_name):
         result = Spacad_api.watch(email=email, signature=self.generate_signature(email=email, salt=salt))
-        Checking.check_status_code(result, 500)
-        actual = Getters.get_json_field_value_0(result, "message")
-        expected = "An application update is required"
-        Checking.assert_values(expected, actual)
-
-    @pytest.mark.parametrize("email, salt", [(EMAIL_SPACAD_WHITELISTED, WEB_SOLT)])
-    def test_no_signature_watch_ad(self, set_spacad_ad, email, salt):
-        result = Spacad_api.watch(email=EMAIL_SPACAD_WHITELISTED, signature=None)
-        Checking.check_status_code(result, 500)
-        actual = Getters.get_json_field_value_0(result, "message")
-        expected = "Update application or refresh the page."
-        Checking.assert_values(expected, actual)
-
+        self.check_watch_result(result, 500, "An application update is required")
+        time.sleep(3)
--- tests/spacAd/test_watch_with_schedule.py
+++ tests/spacAd/test_watch_with_schedule.py
@@ -11,15 +11,16 @@ from utilities.getters import Getters
 
 @pytest.fixture()
 def set_schedule(request):
-    settings, test_name, status_code_eligible, status_code_watch, eligible_field, email, signature = request.param
+    settings, test_name, status_code_eligible, status_code_watch, email, signature = request.param
     # установка расписания
     start_time = datetime.utcnow().strftime("%H:%M:%S")
     end_time = (datetime.utcnow() + timedelta(hours=1)).strftime("%H:%M:%S")
     Spacad_api.set_working_hours(start_time, end_time, [])
     # задаю для этого слота settings
     Spacad_api.set_working_hours(start_time, end_time, settings)
+    time.sleep(3)
     print(f"Расписание установлено для теста: {test_name}")
-    yield status_code_eligible, status_code_watch, eligible_field, email, signature  # возвращаем эти значения для использования их в функции
+    yield status_code_eligible, status_code_watch, email, signature  # возвращаем эти значения для использования их в функции
     Spacad_api.refresh_working_hours("23:00:00", "23:59:59")
     # откат расписания
     print(f"Откатил расписание после теста: {test_name}")
@@ -27,7 +28,7 @@ def set_schedule(request):
 
 @pytest.fixture()
 def set_schedule_closed(request):
-    settings, status_code_eligible, status_code_watch, eligible_field, email, signature = request.param
+    settings, status_code_eligible, status_code_watch, email, signature = request.param
     # установка расписания
     start_time = (datetime.utcnow() + timedelta(hours=1)).strftime("%H:%M:%S")
     end_time = (datetime.utcnow() + timedelta(hours=2)).strftime("%H:%M:%S")
@@ -35,10 +36,42 @@ def set_schedule_closed(request):
     # задаю для этого слота settings
     Spacad_api.set_working_hours(start_time, end_time, settings)
     print(f"Расписание установлено для теста")
-    yield status_code_eligible, status_code_watch, eligible_field, email, signature  # возвращаем эти значения для использования их в функции
+    time.sleep(3)
+    yield status_code_eligible, status_code_watch, email, signature  # возвращаем эти значения для использования их в функции
     Spacad_api.refresh_working_hours("23:00:00", "23:59:59")
     # откат расписания
     print(f"Откатил расписание после теста")
+
+
+@pytest.fixture()
+def set_schedule_params(request):
+    for_adults, collect_coins, test_name, status_code_eligible, \
+        status_code_watch, email, signature, wait_time = request.param
+    # настройки для слота расписания
+    settings = {
+        "actions": [
+            {
+                "action": "invokeCustomEvent",
+                "target": "",
+                "args": "PlaySome",
+                "for_adults": for_adults,
+                "collect_coins": collect_coins
+
+            }
+        ]
+    }
+    # установка расписания
+    start_time = datetime.utcnow().strftime("%H:%M:%S")
+    end_time = (datetime.utcnow() + timedelta(hours=1)).strftime("%H:%M:%S")
+    Spacad_api.set_working_hours(start_time, end_time, [])
+    # задаю для этого слота settings
+    Spacad_api.set_working_hours(start_time, end_time, settings)
+    print(f"Расписание установлено для теста: {test_name}")
+    time.sleep(3)
+    yield status_code_eligible, status_code_watch, email, signature, wait_time  # возвращаем эти значения для использования их в функции
+    Spacad_api.refresh_working_hours("23:00:00", "23:59:59")
+    # откат расписания
+    print(f"Откатил расписание после теста: {test_name}")
 
 
 class TestWatchWithSchedule:
@@ -102,54 +135,91 @@ class TestWatchWithSchedule:
         ]
     }
 
+    @staticmethod
+    def check_is_eligible(email, status_code, result_field_value):
+        # вход в пространство
+        result_eligible = Spacad_api.is_eligible(email=email)
+        Checking.check_status_code(result_eligible, status_code)
+        # проверяем тело ответа
+        expected_result = True
+        actual_result = Getters.get_json_field_value_0(result_eligible, result_field_value)
+        Checking.assert_values(expected_result, actual_result)
+
+    @staticmethod
+    def check_watch(email, signature, status_code):
+        result_watch = Spacad_api.watch(email=email, signature=signature)
+        Checking.check_status_code(result_watch, status_code)
+        if status_code == 200:
+            expected_fields = ['id', 'coins', 'max_limit', 'min_limit', 'coins_left', 'progress', 'expire_at',
+                               'created_at']
+            Checking.check_json_fields_in_v2(result_watch, "data", expected_fields)
+        elif status_code == 403:
+            watch_data = Getters.get_json_field_value_0(result_watch, "message")
+            assert watch_data == "Event doesn't start yet"
+        else:
+            print("Something went wrong!")
+
     @pytest.mark.parametrize("set_schedule",
-                             [(settings_PlaySpacAD, "Test working hours for SpacAd ad", 200, 200, "data",
+                             [(settings_PlaySpacAD, "Test working hours for SpacAd ad", 200, 200,
                                EMAIL_SPACAD_WHITELISTED, WATCH_SIGNATURE),
-                              (settings_PlayGravity, "Test working hours for NONE-SpacAd ad", 200, 403, "data",
+                              (settings_PlayGravity, "Test working hours for NONE-SpacAd ad", 200, 403,
                                EMAIL_SPACAD_WHITELISTED, WATCH_SIGNATURE),
-                              (settings_PlayNothing, "Test working hours without ad", 200, 403, "data",
+                              (settings_PlayNothing, "Test working hours without ad", 200, 403,
                                EMAIL_SPACAD_WHITELISTED, WATCH_SIGNATURE),
-                              (settings_PlayAdult, "Test watch adult ad with KYC", 200, 200, "data",
+                              (settings_PlayAdult, "Test watch adult ad with KYC", 200, 200,
                                EMAIL_SPACAD_KYC, SPACAD_KYC_SIGNATURE),
-                              (settings_PlayAdult, "Test watch adult without KYC", 200, 403, "data",
+                              (settings_PlayAdult, "Test watch adult without KYC", 200, 403,
                                EMAIL_SPACAD_WHITELISTED, WATCH_SIGNATURE)
                               ],
                              indirect=True)
     def test_watch_and_access_critical_path(self, set_schedule):
-        status_code_eligible, status_code_watch, eligible_field, email, signature = set_schedule  # определяем значения переменных из фикстуры
+        status_code_eligible, status_code_watch, email, signature = set_schedule  # определяем значения переменных из фикстуры
         # вход в пространство
-        result_eligible = Spacad_api.is_eligible(email=email)
-        Checking.check_status_code(result_eligible, status_code_eligible)
-        # проверяем тело ответа
-        expected_result = True
-        actual_result = Getters.get_json_field_value_0(result_eligible, eligible_field)
-        Checking.assert_values(expected_result, actual_result)
+        self.check_is_eligible(email, status_code_eligible, "data")
 
         # сбор монеты
-        result_watch = Spacad_api.watch(email=email, signature=signature)
-        Checking.check_status_code(result_watch, status_code_watch)
+        self.check_watch(email, signature, status_code_watch)
         time.sleep(60)  # ожидание перед каждым сбором для 100% избежания конфликта сбора монет
 
     @pytest.mark.parametrize("set_schedule_closed",
-                             [(settings_PlaySpacAD, 403, 403, "is_finished",
+                             [(settings_PlaySpacAD, 403, 403,
                                EMAIL_SPACAD_WHITELISTED, WATCH_SIGNATURE)], indirect=True)
     def test_access_schedule_closed(self, set_schedule_closed):
-        status_code_eligible, status_code_watch, eligible_field, email, signature = set_schedule_closed  # определяем значения переменных из фикстуры
+        status_code_eligible, status_code_watch, email, signature = set_schedule_closed  # определяем значения переменных из фикстуры
         # вход в пространство
-        result_eligible = Spacad_api.is_eligible(email=email)
-        Checking.check_status_code(result_eligible, status_code_eligible)
-        # проверяем тело ответа
-        expected_result = True
-        actual_result = Getters.get_json_field_value_0(result_eligible, eligible_field)
-        Checking.assert_values(expected_result, actual_result)
+        self.check_is_eligible(email, status_code_eligible, "is_finished")
 
         # сбор монеты
         result_watch = Spacad_api.watch(email=email, signature=signature)
         Checking.check_status_code(result_watch, status_code_watch)
-        # тут нужно проверить респонс что event Event doesn't start yet.!!!
+        watch_data = Getters.get_json_field_value_0(result_watch, "message")
+        assert "Event doesn't start yet. Come after 59 minutes" in watch_data
 
-    def test_collect_coins(self):
-        pass
+    @pytest.mark.parametrize("set_schedule_params",
+                             [("1", "1", "Test adults=1 collect=1 for user with KYC", 200, 200, EMAIL_SPACAD_KYC,
+                               SPACAD_KYC_SIGNATURE, 1),
+                              ("1", "1", "Test adults=1 collect=1 for user without KYC", 200, 403, EMAIL_SPACAD_WHITELISTED,
+                               WATCH_SIGNATURE, 59),
+                              ("1", "0", "Test adults=1 collect=0 for user with KYC", 200, 403, EMAIL_SPACAD_KYC,
+                               SPACAD_KYC_SIGNATURE, 1),
+                              ("1", "0", "Test adults=1 collect=0 for user without KYC", 200, 403, EMAIL_SPACAD_WHITELISTED,
+                               WATCH_SIGNATURE, 59),
+                              ("0", "1", "Test adults=0 collect=1 for user with KYC", 200, 200, EMAIL_SPACAD_KYC,
+                               SPACAD_KYC_SIGNATURE, 1),
+                              ("0", "1", "Test adults=0 collect=1 for user without KYC", 200, 200, EMAIL_SPACAD_WHITELISTED,
+                               WATCH_SIGNATURE, 59),
+                              (None, "0", "Test adults=Null collect=0 for user with KYC", 200, 403, EMAIL_SPACAD_KYC,
+                               SPACAD_KYC_SIGNATURE, 1),
+                              ("0", None, "Test adults=0 collect=Null for user without KYC", 200, 403, EMAIL_SPACAD_WHITELISTED,
+                               WATCH_SIGNATURE, 59),
+                              ],
+                             indirect=True)
+    def test_settings_args(self, set_schedule_params):
+        status_code_eligible, status_code_watch, email, signature, wait_time = set_schedule_params  # определяем значения переменных из фикстуры
+        # вход в пространство
+        self.check_is_eligible(email, status_code_eligible, "data")
 
-    def test_for_adults(self):
-        pass
+        # сбор монеты
+        self.check_watch(email, signature, status_code_watch)
+
+        time.sleep(wait_time)  # ожидание перед каждым сбором для 100% избежания конфликта сбора монет
