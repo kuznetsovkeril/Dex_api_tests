a7698a427743fc483ff7dd89e6e56486ce3f4815
kuznetsovkeril <kuznetsovkeril@gmail.com>
2023-11-19 21:05:55 +0300

    ui energy tests

--- tests_ui/energy_units/test_energy_landing.py
+++ tests_ui/energy_units/test_energy_landing.py
@@ -39,12 +39,14 @@ class TestEnergyUnitsPage:
         expect(page1).to_have_title("DEXART Metaverse Tokens Staking")
 
     # check if
-    def test_set_email_and_auth_on_landing(self, browser_page):
+    def test_set_email_on_landing(self, browser_page):
         email = "test@fexbox.org"
         # open browser and login
         page = browser_page
         lp = LoginPage(page, BASE_URL)
         lp.email_login(email=email, password="1qazXSW@")
+
+        # get auth token
 
         # go to TPF and go to Energy landing
         page.goto(BASE_URL + "/balance")
@@ -58,19 +60,49 @@ class TestEnergyUnitsPage:
         # accept cookies
         page1.get_by_role("button", name="Accept Cookies").click()
         # pick up first package
-        page1.locator(".packagesBox__plusMinus").first.click()
-        time.sleep(10)
+        page1.click('div[class="packagesBox__plusMinus"]')
         # click button buy -> the form opened
         page1.get_by_role("button", name="Buy").click()
 
         # assertions
+        # 1) check if the email is set in email input
         time.sleep(1)
+        # get value email field with JavaScript
         email_input_vale = page1.evaluate('''() => {
         const input = document.querySelector('input[type="email"]');
         return input ? input.value : null;
     }''')
         print(email_input_vale)
         assert email_input_vale == email
+
+        # 2) auth is set
+        page1.on("request", lambda request: print(">>", request.method, request.url))
+
+    # check the auth token routed in order energy request
+    def test_set_auth_on_landing(self, browser_page):
+        page = browser_page
+        page.goto(f'{BASE_ENERGY_URL}/?email=kirtest@fexbox.org&token={AUTH_KIRTEST}')
+        page.get_by_role("button", name="Accept Cookies").click()
+
+        # accept cookies
+        page.get_by_role("button", name="Accept Cookies").click()
+        # pick up first package
+        page.click('div[class="packagesBox__plusMinus"]')
+        # click button buy -> the form opened
+        page.get_by_role("button", name="Buy").click()
+        # pay modal opens
+        page.get_by_text("Choose payment method").click()
+        page.get_by_text("Crypto wallet").click()
+        page.locator("label").click()
+        page.get_by_role("dialog").get_by_role("button", name="Buy").click()
+
+        # get auth header value in the order request
+        with page.expect_request("https://stacking-api.108dev.space/api/orders") as response_info:
+            page.get_by_role("dialog").get_by_role("button", name="Buy").click()
+        request = response_info.value
+        header_value = request.header_value("authorization")
+        # assert request header with auth token
+        assert header_value == f'Bearer {AUTH_KIRTEST}'
 
     # check if the email input is disabled
     def test_email_input_disabled(self, browser_page):
@@ -84,3 +116,10 @@ class TestEnergyUnitsPage:
 
         # also possible code: assert page.get_by_placeholder("Your email").is_disabled()
         assert page.is_disabled('input[type="email"]')
+
+    def test_go_to_direct_energy_link(self, browser_page):
+        page = browser_page
+        page.goto(BASE_ENERGY_URL)
+        page.get_by_role("button", name="Accept Cookies").click()
+        # check if the purchase is not available
+        page.get_by_role("heading", name="Energy Units sale is over").is_visible()
